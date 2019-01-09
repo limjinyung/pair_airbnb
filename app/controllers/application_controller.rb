a884907ad3748924a33a99824e229b30f39ae7d0
limjy0114 <jinyung0069@hotmail.com>
2019-01-09 23:29:25 +0800

    installed Figaro Gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  def index
+  end
+
+  # def new
+  # end
+
+  # def create
+  # 	@user = User.new(params[:user])
+  # 	if @user.save 
+  # 		redirect_to user_path(@user.id)
+  # 	else
+  # 		render :new
+  # 	end
+  # end
 end
